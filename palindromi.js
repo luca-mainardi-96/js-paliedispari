de1830c6027ed7cd9f324786e18bc8c4e7fad17b
luca-mainardi-96 <luca.mainardi.12358@gmail.com>
2025-05-21 22:36:39 +0200

    primo esercizio

--- palindromi.js
+++ palindromi.js
@@ -2,3 +2,26 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
+//richiesta all'utente
+
+let parolaUtente = prompt('Digita una parola')
+
+isPalindrome(parolaUtente)
+
+//creazione funzione
+
+function isPalindrome(parolaUtente){
+    const parolaMin = parolaUtente.toLocaleLowerCase()
+    const parolaInvertita = parolaMin.split('').reverse().join('')
+    const ePalindromo = true
+    if (parolaMin === parolaInvertita){
+        console.log(ePalindromo)
+    } else{
+        const ePalindromo = false
+        console.log(ePalindromo)
+    }
+}
+
+
+
+
