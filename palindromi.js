3a03430a5d114e6f766c857cf16b979bdaf964e0
luca-mainardi-96 <luca.mainardi.12358@gmail.com>
2025-05-22 22:15:21 +0200

    correzione es 1

--- palindromi.js
+++ palindromi.js
@@ -1,19 +1,51 @@
-let parolaUtente = prompt('Digita una parola')
+//Mia Versione
+
+const parolaUtente = prompt('Digita una parola')
 
 isPalindrome(parolaUtente)
 
-function isPalindrome(parolaUtente){
-    const parolaMin = parolaUtente.toLocaleLowerCase()
-    const parolaInvertita = parolaMin.split('').reverse().join('')
-    const ePalindromo = true
-    if (parolaMin === parolaInvertita){
-        console.log(ePalindromo)
-    } else{
-        const ePalindromo = false
-        console.log(ePalindromo)
-    }
-}
+function isPalindrome(parola){
+     const parolaMin = parola.toLocaleLowerCase()
+     const parolaInvertita = parolaMin.split('').reverse().join('')
+     const ePalindroma = true
+     if (parolaMin === parolaInvertita){
+         console.log(ePalindroma, 'La parola è palindroma')
+     } else{
+         const ePalindromo = false
+         console.log(ePalindroma, 'La parola non è palindroma')
+     }
+ }
 
 
+// Correzione
 
+
+// const parolaUtente = prompt('Insertisci una parola')
+
+// if (ePalindroma(parolaUtente)){
+//     console.log('Si')
+// } else {
+//     console.log('No')
+// }
+
+// function ePalindroma(parola) {
+//     parola = parola.toLowerCase()
+//     const parolaInvertita = invertiParola(parola)
+//     if (parolaInvertita === parola){
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+
+// function invertiParola(parola){
+//     let parolaInvertita = ''
+
+//     for (let i=0; i<parola.length; i++){
+//         const char = parola.at(-1 -i)
+//         parolaInvertita += char
+//     }
+//     return parolaInvertita
+// }
 
